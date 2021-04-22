660b713a7fae897e2bf3cb9de2de4decc1c7ef01
stackdynamic <stackdynamic@gmail.com>
2021-04-21 23:12:04 -0500

    Parsing for graphs

---
+++ src/core/graph.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <algorithm>
+#include "graph.h"
+#include <boost/algorithm/string.hpp>
+#include "../util/util.h"
+
+using std::stringstream;
+using std::getline;
+using namespace wikigraph::util;
+
+namespace wikigraph {
+namespace core {
+
+Graph::Graph(size_t lowerIndex, size_t higherIndex, const string &filename)
+    : _lowerIndex(lowerIndex), _higherIndex(higherIndex) {
+    // initialize with empty nodes
+    for (size_t i = lowerIndex; i < higherIndex; i++)
+        nodes.push_back(new Node(i));
+
+    // validate file
+    if (!file_exists(filename))
+        throw std::invalid_argument("File " + filename + " does not exist");
+    ifstream ifs(filename);
+    string line;
+
+    // iterate over file
+    while (getline(ifs, line)) {
+        // split line
+        vector<string> split;
+        boost::split(split, line, " ");
+        size_t from = stoi(split[0]);
+        size_t to = stoi(split[1]);
+
+        addEdge(from, to);
+    }
+}
+
+void Graph::addEdge(size_t from, size_t to) {
+    // validate node indices
+    if (!isValid(_lowerIndex, _higherIndex, from) || !isValid(_lowerIndex, _higherIndex, to))
+        throw std::invalid_argument("Received node index out of range");
+    from -= _lowerIndex;
+    to -= _lowerIndex;
+    nodes[from]->neighbors.push_back(nodes[to]);
+}
+
+}
+}
---
+++ src/core/graph.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+using std::vector;
+using std::string;
+
+namespace wikigraph {
+namespace core {
+
+class Graph {
+public:
+    /**
+     * Node struct which stores an integer index and a vector of neighbors.
+     */
+    struct Node {
+        Node(size_t index) : index(index) {}
+        size_t index;
+        vector<Node *> neighbors;
+    };
+    /**
+     * Constructor for a Graph object.
+     *
+     * @param lowerIndex the lowest node index
+     * @param higherIndex the highest node index
+     * @param filename path to output of prep-data
+    */
+    Graph(size_t lowerIndex, size_t higherIndex, const string &filename);
+
+    vector<Node *> nodes;
+private:
+    /**
+     * Helper method to insert edge into the graph.
+     * @param from index of the node the edge is going from
+     * @param to index of the node the edge is going to
+     */
+    void addEdge(size_t from, size_t to);
+    size_t _lowerIndex;
+    size_t _higherIndex;
+};
+
+}
+}
