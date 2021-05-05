b439a59338999c98dbddd6e0e7b8f950884e79e4
stackdynamic <stackdynamic@gmail.com>
2021-05-05 05:03:04 -0500

    Add particle class

---
+++ src/core/particle.cpp
@@ -0,0 +1,32 @@
+#include "particle.h"
+
+namespace wikigraph {
+namespace core {
+
+Particle::Particle(const dvec2& position, const dvec2& velocity, const dvec2& acceleration, double mass) : position(
+    position), velocity(velocity), acceleration(acceleration), mass(mass) {}
+
+void Particle::addForce(const dvec2& forceVector) {
+    acceleration += forceVector / mass;
+}
+
+void Particle::update(double dt) {
+    velocity += acceleration * dt;
+    position += velocity * dt;
+    acceleration = {0, 0};
+}
+
+double distance(const Particle& p1, const Particle& p2) {
+    return glm::distance(p1.position, p2.position);
+}
+
+double distance2(const Particle& p1, const Particle& p2) {
+    return glm::distance2(p1.position, p2.position);
+}
+
+dvec2 towards(const Particle& p1, const Particle& p2) {
+    return glm::normalize(p2.position - p1.position);
+}
+
+}
+}
---
+++ src/core/particle.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#define GLM_ENABLE_EXPERIMENTAL
+#include <vector>
+#include <string>
+#include <glm/glm.hpp>
+#include <glm/gtx/norm.hpp>
+
+using std::vector;
+using std::string;
+using glm::dvec2;
+
+namespace wikigraph {
+namespace core {
+
+/**
+ * Represents a particle with mass.
+ */
+class Particle {
+public:
+    dvec2 position;
+    dvec2 velocity;
+    dvec2 acceleration;
+    const double mass;
+    Particle(const dvec2& position, const dvec2& velocity, const dvec2& acceleration, double mass);
+    /**
+     * Adds a force to the particle for the current timestep (not persistent).
+     *
+     * @param forceVector vector representing the force
+     */
+    void addForce(const dvec2& forceVector);
+    /**
+     * Updates the position and velocity of the particle, resets the acceleration.
+     *
+     * @param dt the timestep
+     */
+    void update(double dt);
+};
+
+/**
+ * Helper function to compute distance between two particles.
+ */
+double distance(const Particle& p1, const Particle& p2);
+
+/**
+ * Helper function to compute square distance between two particles.
+ */
+double distance2(const Particle& p1, const Particle& p2);
+
+/**
+ * Helper function to compute the unit vector pointing from p1 to p2.
+ */
+dvec2 towards(const Particle& p1, const Particle& p2);
+
+}
+}
